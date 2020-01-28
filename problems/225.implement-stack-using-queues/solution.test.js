329b7da6820de3c12522587e6a39ec8c419f00c1
zhijiang3 <240897357@qq.com>
2020-01-28 21:24:15 +0800

    test: update test case for problem 225.implement-stack-using-queues

--- problems/225.implement-stack-using-queues/solution.test.js
+++ problems/225.implement-stack-using-queues/solution.test.js
@@ -1,6 +1,16 @@
 import MyStack from "./solution.js";
 
-test("example 1", () => {
+test("入栈和出栈", () => {
+  const stack = new MyStack();
+
+  stack.push(1);
+  stack.push(2);
+
+  expect(stack.pop()).toBe(2);
+  expect(stack.pop()).toBe(1);
+});
+
+test("查看栈顶元素", () => {
   const stack = new MyStack();
 
   stack.push(1);
@@ -8,5 +18,20 @@ test("example 1", () => {
 
   expect(stack.top()).toBe(2);
   expect(stack.pop()).toBe(2);
-  expect(stack.empty()).toBe(false);
+});
+
+test("检查是否为空", () => {
+  const stack = new MyStack();
+
+  expect(stack.empty()).toBe(true);
+
+  stack.push(1);
+  stack.push(2);
+
+  expect(stack.empty()).not.toBe(true);
+
+  stack.pop();
+  stack.pop();
+
+  expect(stack.empty()).toBe(true);
 });
