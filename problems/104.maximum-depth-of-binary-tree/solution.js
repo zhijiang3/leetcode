6d9dda3d39bdcec2b4c1bc200a8da42bec563eda
zhijiang3 <240897357@qq.com>
2020-05-07 17:13:05 +0800

    feat: add 104

---
+++ problems/104.maximum-depth-of-binary-tree/solution.js
@@ -0,0 +1,12 @@
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+export default function maxDepth(root) {
+  if (!root) return 0;
+
+  const leftDepth = maxDepth(root.left);
+  const rightDepth = maxDepth(root.right);
+
+  return Math.max(leftDepth, rightDepth) + 1;
+}
