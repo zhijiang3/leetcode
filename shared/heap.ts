9e56228d037d41cba4dbd093c22b2c97e8e9cc47
志江 <zhijiang34@gmail.com>
2023-10-18 09:54:52 +0800

    fix(heap): heapDown not work

--- shared/heap.ts
+++ shared/heap.ts
@@ -2,10 +2,13 @@ import { swap } from './swap';
 
 export function heapUp<T>(heap: T[], heapIndex: number) {
   while (heapIndex > 0) {
-    const parentIndex = Math.floor((heapIndex - 1) / 2);
+    const parentIndex = (heapIndex - 1) >> 1;
 
-    // 如果是大根堆，这里的条件需要调整
-    if (heap[heapIndex] > heap[parentIndex]) break;
+    // 大根堆
+    // if (heap[heapIndex] <= heap[parentIndex]) break;
+
+    // 小根堆
+    if (heap[heapIndex] >= heap[parentIndex]) break;
 
     swap(heap, heapIndex, parentIndex);
 
@@ -18,20 +21,23 @@ export function heapDown<T>(heap: T[], heapIndex: number) {
 
   let leftIndex = 2 * heapIndex + 1;
   let rightIndex = 2 * heapIndex + 2;
-  let targetIndex: number;
 
   while (leftIndex < n) {
-    // 如果是大根堆，这里的条件需要调整
+    // 大根堆
+    // if (rightIndex < n && heap[rightIndex] > heap[leftIndex]) {
+    //   leftIndex = rightIndex;
+    // }
+    // if (heap[leftIndex] <= heap[heapIndex]) break;
+
+    // 小根堆
     if (rightIndex < n && heap[rightIndex] < heap[leftIndex]) {
-      targetIndex = rightIndex;
-    } else {
-      targetIndex = leftIndex;
+      leftIndex = rightIndex;
     }
+    if (heap[leftIndex] >= heap[heapIndex]) break;
 
-    // 如果是大根堆，这里的条件需要调整
-    if (heap[targetIndex] > heap[heapIndex]) break;
+    swap(heap, heapIndex, leftIndex);
 
-    swap(heap, heapIndex, targetIndex);
+    heapIndex = leftIndex;
 
     leftIndex = 2 * targetIndex + 1;
     rightIndex = 2 * targetIndex + 2;
